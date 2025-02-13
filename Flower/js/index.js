a9ac4e7e320da74bd25bde8ab6b366f6559dc32a
TRESE <115089169+trese24@users.noreply.github.com>
2025-02-13 23:39:49 +0800

    Update index.js

--- Flower/js/index.js
+++ Flower/js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something To Tell you "Click Heart"'.split('')
+const text = 'I Have Something To Tell you "Click The Heart"'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
